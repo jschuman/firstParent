473d519f014489edc4b72d06b7ae3924c1b84f8e
Jeff Schuman <JeffSchuman@deltek.com>
2019-07-10 15:00:20 -0400

    now injects script into page DOM; works with url changes such as back, forward in the browser and links within TFS

--- loader.js
+++ loader.js
@@ -1,24 +1,8 @@
-// https://developer.chrome.com/extensions/content_scripts
-var deBounce = 1000;
-var doIt = function() { 
-    var ddwn = document.getElementById("filter-dropdown-option");
-    if (!ddwn) { 
-        console.log("FirstParent: filter not visible yet");
-        deBounce += 1000;
-        if (deBounce < 5000) {
-            setTimeout(doIt, deBounce);
-        }
-        return;
-    }
-    if (ddwn.children[0].innerHTML == 'First parent') { console.log("already set."); return ;}
-    ddwn.click();
-    var firstParent = document.getElementsByClassName("ms-Dropdown-optionText optionText_8ad48cc5")[1]; // second item in list
-    firstParent.click();
-    setTimeout(scrollBottom, 500);
- }
- var scrollBottom = function() {
-     var bottom = 20000;
-     document.getElementsByClassName("vc-hl-auto-scroll-absolute")[0].scrollTop = bottom;
-     // todo: keep looping until loaded all
- }
-setTimeout(doIt, deBounce);
+//used to inject script.js into the dom
+// see https://stackoverflow.com/a/9310273
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('script.js');
+(document.head||document.documentElement).appendChild(s);
+s.onload = function() {
+    s.parentNode.removeChild(s);
+};
---
+++ script.js
@@ -0,0 +1,34 @@
+// https://developer.chrome.com/extensions/content_scripts
+var deBounce = 1000;
+var doIt = function() { 
+    var ddwn = document.getElementById("filter-dropdown-option");
+    if (!ddwn) { 
+        console.log("FirstParent: filter not visible yet");
+        deBounce += 1000;
+        if (deBounce < 5000) {
+            setTimeout(doIt, deBounce);
+        }
+        return;
+    }
+    if (ddwn.children[0].innerHTML == 'First parent') { console.log("already set."); return ;}
+    ddwn.click();
+    var firstParent = document.getElementsByClassName("ms-Dropdown-optionText optionText_8ad48cc5")[1]; // second item in list
+    firstParent.click();
+    setTimeout(scrollBottom, 500);
+ }
+ var scrollBottom = function() {
+     var bottom = 20000;
+     document.getElementsByClassName("vc-hl-auto-scroll-absolute")[0].scrollTop = bottom;
+     // todo: keep looping until loaded all
+ }
+setTimeout(doIt, deBounce);
+
+
+//popstate works for back/forward button in browser, as well as url change
+//from user action in TFS
+window.onpopstate = function(event) {
+  console.log(`onpopstate location: : ${window.location}`);
+  setTimeout(doIt, 1000);
+};
+
+
